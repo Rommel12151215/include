fe689280304fe6ead0096b0b0a873d496d3c764b
Rommel <karlsrommel255@gmail.com>
2023-11-04 23:52:46 -0600

    include

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+    include("header.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This is the Home page<br>
+    Stuff about your home page can go here<br>
+</body>
+</html>
+<?php
+    include("footer.html");
+?>
+
+<header>
+    <h2>This is my website</h2>
+    <a href="index.php">Home</a>
+    <a href="about.php">About</a>
+    <a href="locations.php">Locations</a>
+    <hr>
+</header>
+
+<footer>
+    <hr>
+    Author: karlsRomm@fake.com 
+    <a href="mailto:karlsRomm@fake.com">karlsRomm@fake.com</a>
+</footer>
